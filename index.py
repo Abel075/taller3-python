1635dcc50117f9619643d2ba6e796e414269297e
Abel075 <abeld.correa@gmail.com>
2021-05-20 02:15:13 -0300

    inicio app

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template #metodo para retornar la plantilla en template
+
+#Archivo principal de la aplicacion
+app = Flask(__name__)
+
+#Rutas @
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return 'About'
+
+
+#Archivo que arranca la aplicacion
+if __name__ == '__main__':
+    app.run()
