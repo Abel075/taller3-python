90f942cd161d0b1c8cdaf0b34e461a9912ac033a
Abel075 <abeld.correa@gmail.com>
2021-05-20 17:21:07 -0300

    actualizacion layout

--- index.py
+++ index.py
@@ -1,19 +1,45 @@
 from flask import Flask
 from flask import render_template #metodo para retornar la plantilla en template
+#from flask_sqlalchemy import SQLAlchemy  #Libreria de base de datos
+
+import os
+
+#dbdir = "sqlite:///" +os.path.abspath(os.gtwcwd()) + "/database.db"
 
 #Archivo principal de la aplicacion
 app = Flask(__name__)
+#Base de datos
+# app.config["SQLAlchemy_DATABASE_URI"] = dbdir
+# db = SQLAlchemy(app)
 
 #Rutas @
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/hardware')
+def hardware():
+    return render_template('hardware.html')
 
+@app.route('/armado_de_pc')
+def armado():
+    return render_template('arma_tu_pc.html')
 
+@app.route('/pc_armadas')
+def armadas():
+    return render_template('pc_armadas.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/mi_cuenta')
+def mi_cuenta():
+    return render_template('mi_cuenta.html')
+
+@app.route('/ayuda')
+def ayuda():
+    return render_template('ayuda.html')
 
 
 #Archivo que arranca la aplicacion
